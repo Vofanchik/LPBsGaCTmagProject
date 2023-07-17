7d21b94cd0a815ae8613043613e5acb93ee6ad6c
Vladimir Kalinin <Vofanchic@mail.ru>
2023-07-17 22:33:33 +0500

    Hint in C_char error config.py

--- Classes/config.py
+++ Classes/config.py
@@ -1,2 +1,4 @@
 jchempaint_path: str = './external/JChemPaint.jar'
 packnames_for_r = ('ggplot2', 'openxlsx', 'sjPlot', 'magrittr', 'drc', 'Rmisc', 'data.table')
+
+# заменить _CCHAR_ENCODING = 'cp1252' в venv/Lib/site-packages/rpy2/rinterface_lib/callbacks.py
